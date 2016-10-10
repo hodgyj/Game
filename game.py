01c3c9354aec2e55804a4cefbf02a5dd7dcd3d39
James Hodgson <hodgyj@outlook.com>
2016-10-10 21:12:17 +0100

    Added python joke

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
+import time
 def main():
-    print("Yaay")
-
+    print("""'python' is not recognized as an internal or external command,
+operable program or batch file.""")
+    time.sleep(4)
+    print("lol jk")
 if __name__ == "__main__":
     main()
