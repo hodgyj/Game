98c0e594f31e36dc214cada2d670655bd766f03b
James Hodgson <hodgyj@outlook.com>
2016-10-11 13:14:48 +0100

    Add game files

--- game.py
+++ game.py
@@ -1,4 +1,9 @@
 import time
+from map import rooms
+from player import *
+from items import *
+from gameparser import *
+
 def main():
     print("""'python' is not recognized as an internal or external command,
 operable program or batch file.""")
