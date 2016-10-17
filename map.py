983f2ea5b00f60a4e0fd430f7a5e59d8314b3906
James Hodgson <hodgyj@outlook.com>
2016-10-17 12:48:44 +0100

    Update map

--- map.py
+++ map.py
@@ -1,81 +1,61 @@
-from items import *
+room_office={
+	"name":"kirills office",
 
-room_reception = {
-    "name": "Reception",
+	"description": 
+	"""you have woken in a moderatly decorated office. The absence of overhead light casts shadows into the corners,
+spilling under the desk and over top the framed black-and-white pictures on the wall. A desk stands before you layden
+with books and papers all of incomprehensible text """,
 
-    "description":
-    """You are in a maze of twisty little passages, all alike.
-Next to you is the School of Computer Science and
-Informatics reception. The receptionist, Matt Strangis,
-seems to be playing an old school text-based adventure
-game on his computer. There are corridors leading to the
-south and east. The exit is to the west.""",
+	"exits": {"west": "Corridor","south": "exit"}
 
-    "exits": {"south": "Admins", "east": "Tutor", "west": "Parking"},
+	}
 
-    "items": [item_biscuits, item_handbook]
+room_boss= {
+	"name":"boss room",
+        
+	"description":
+	"""water trickles down the stone walls that are coated in a suspiciously viscid substance, a mass of darkness begins
+to manifest before your eyes thickening becoming the embodiment of your worst nightmares. The mass' voice beckons demanding
+you drop your weapon and surrender""",
+
+	"exits": {"south": "Corridor"}
+
+	}
+
+Armoury = {
+	"name":"armoury",
+
+	"description":
+	"""you are in the armoury""",
+
+	"exits": {"east": "Corridor", "south": "emty"}
 }
 
-room_admins = {
-    "name": "MJ and Simon's room",
+empty_room ={
+	"name":"empty",
+        
+	"description":
+	"""you have entered an empty room""",
 
-    "description":
-    """You are leaning agains the door of the systems managers'
-room. Inside you notice Matt "MJ" John and Simon Jones. They
-ignore you. To the north is the reception.""",
+	"exits": {"north": "armoury"}
 
-    "exits":  {"north": "Reception"},
-
-    "items": []
 }
 
-room_tutor = {
-    "name": "your personal tutor's office",
+room_death ={
+	"name":"room of death",
 
-    "description":
-    """You are in your personal tutor's office. He intently
-stares at his huge monitor, ignoring you completely.
-On the desk you notice a cup of coffee and an empty
-pack of biscuits. The reception is to the west.""",
+	"description":
+	"""you are in the death room""",
 
-    "exits": {"west": "Reception"},
-
-    "items": []
+	"exits": {"south west": "Corridor"}
 }
 
-room_parking = {
-    "name": "the parking lot",
+corridor ={
+	"name":"Corridor",
 
-    "description":
-    """You are standing in the Queen's Buildings parking lot.
-You can go south to the COMSC reception, or east to the
-general office.""",
+	"description":
+	"""you have stumbled out of kirills office into a long forgotten under ground tunnel, dim lighting casts a shadow on the wooden doors spaced every so often.
+you can exit north to the boss' room, north east to the room of death, east back to kirills office and west to the armoury""",
 
-    "exits": {"east": "Office", "south": "Reception"},
-
-    "items": []
-}
-
-room_office = {
-    "name": "the general office",
-
-    "description":
-    """You are standing next to the cashier's till at
-30-36 Newport Road. The cashier looks at you with hope
-in their eyes. If you go west you can return to the
-Queen's Buildings.""",
-
-    "exits": {"west": "Parking"},
-
-    "items": [item_pen]
-}
-
-
-
-rooms = {
-    "Reception": room_reception,
-    "Admins": room_admins,
-    "Tutor": room_tutor,
-    "Parking": room_parking,
-    "Office": room_office
+	"exits": {"north": "boss room", " north east": "room of death", "east": "kirills office", "west": "armoury"}
 }
