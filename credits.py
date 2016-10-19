d5dba098a25b314c1cc5e8bede7094c63a638b51
Gitcatsquid <almorgan@live.com>
2016-10-19 11:04:21 +0100

    Fuck you Luca

--- credits.py
+++ credits.py
@@ -6,10 +6,11 @@ def roll_credits():
 	"\nDesigned by: gods perfect idiot",
 	"\nProduced by: the real heroes here",
 	"\nCharacter design: James",
-	"\nNarrator design: Luca",
-	"\nAnnoying publisher who doesn't understand development time: Alastair",
-	"\nInsult co-ordinator: Alastair",
-	"\nitem creators: Nick, Natalie, Sam.",
+	"\nNarrator design: Luca, Alastair",
+	"\nMurderers: Alastair, Luca, Dervla",
+	"\nInsult co-ordinator: Alastair, Luca, James, Dervla",
+	"\nWorld creators: creators: Nick, Natalie, Sam, Alastair",
+	"\nLab rats: Nick, Alastair, Sam, Natalie",
 	"\nGame coders: Dervla, James, Luca",
 	"\nSpecial thanks to Louie for being the hardest working member of the team."]
 
