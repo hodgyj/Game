5bcd50ca75df3499481cac7f4525e3be541127e5
Gianluca Gordon <ggordon173@gmail.com>
2016-10-19 10:56:25 +0100

    insulted alastair

--- credits.py
+++ credits.py
@@ -6,8 +6,9 @@ def roll_credits():
 	"\nDesigned by: gods perfect idiot",
 	"\nProduced by: the real heroes here",
 	"\nCharacter design: James",
-	"\nnarrator design: Luca",
-	"\nInsult co-ordinator: Alistair",
+	"\nNarrator design: Luca",
+	"\nAnnoying publisher who doesn't understand development time: Alastair",
+	"\nInsult co-ordinator: Alastair",
 	"\nitem creators: Nick, Natalie, Sam.",
 	"\nGame coders: Dervla, James, Luca",
 	"\nSpecial thanks to Louie for being the hardest working member of the team."]
